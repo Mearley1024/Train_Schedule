da260ae2d04c5f50ca31de22594d3292ea2f5db8
Marcela Earley <Marcela.earley1024@gmail.com>
2019-09-04 20:19:36 -0700

    updates to file

---
+++ Train Schedule/logic.js
@@ -0,0 +1,12 @@
+var config = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    storageBucket: "",
+    messagingSenderId: "",
+};
+
+firebase.initializeApp(config);
+
+var trainData = firebase.database();
+
